bbe73e9db5ebd2277c2fccdaac19cbedc47fe24a
antonwallstedt <antonwallstedt@outlook.com>
2024-04-06 22:40:08 +0200

    fix(styling): temporarily disable dark mode and minor styling changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,7 +10,7 @@ export default function AdminPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+        <div className="flex h-16 w-full items-end rounded-lg from-blue-400 to-blue-500 bg-gradient-to-tr p-3 md:h-16">
           <div className="w-32 text-white md:w-36">
             <AntonLogo />
           </div>
--- src/app/ui/antonLogo.tsx
+++ src/app/ui/antonLogo.tsx
@@ -3,7 +3,7 @@ import { UserIcon } from "@heroicons/react/24/outline";
 
 export default function AntonLogo() {
   return (
-    <div className="flex flex-row items-center leading-none text-white">
+    <div className="flex flex-row items-center leading-none text-white drop-shadow-lg">
       <LockClosedIcon className="h-10 w-10 mr-3" />
       <p className="text-[32px] font-semibold">Anton</p>
     </div>
