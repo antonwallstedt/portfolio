5f6d4df06e3324197bddaf8a0191f06a23749433
antonwallstedt <antonwallstedt@outlook.com>
2024-04-08 21:59:34 +0200

    feat(style): add new colour scheme to dashboard

--- src/app/ui/AdminDash/navLinks.tsx
+++ src/app/ui/AdminDash/navLinks.tsx
@@ -21,12 +21,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                "bg-sky-100 text-blue-600": pathname === link.href,
-              }
-            )}
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md interactive-bg-surface-variant p-3 text-sm font-medium  md:flex-none md:justify-start md:p-2 md:px-3", {
+              "bg-tertiary-container text-on-tertiary-container": pathname === link.href,
+            })}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
