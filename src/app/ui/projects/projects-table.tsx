e4b3c396a71b435433f7f3d13f4b01a2f8f1e211
antonwallstedt <antonwallstedt@outlook.com>
2024-04-15 21:52:57 +0200

    chore: add alt to images

--- src/app/ui/projects/projects-table.tsx
+++ src/app/ui/projects/projects-table.tsx
@@ -8,7 +8,7 @@ export default async function ProjectsTable() {
     <div className="mt-6 flex flex-wrap justify-center items-center w-full p-5 rounded-md">
       {projects?.map((project) => (
         <div key={project.id} className="flex m-5 bg-neutral-100 rounded-md p-3 drop-shadow-md w-[75%]">
-          <img src={project.imageSrc} width={200} height={200} className="rounded-md" />
+          <img src={project.imageSrc} width={200} height={200} className="rounded-md" alt={`Image of ${project.title}`} />
           <div className="ml-5 relative drop-shadow-sm">
             <h1 className="text-xl font-medium mb-2">{project.title}</h1>
             <p className="text-lg mb-4">{project.subtitle}</p>
